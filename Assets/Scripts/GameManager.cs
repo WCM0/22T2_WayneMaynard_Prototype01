c878f35d23e6dc9f057b51ec9504bd0fe0734671
pabloalv96 <1022923@student.sae.edu.au>
2022-06-22 21:46:33 +1000

    Changes in the Berries position, UI which updates the collectibles, and well which spawns the key

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public TMPro.TMP_Text textMeshPro;
+    public TMPro.TMP_Text berryText;
+    public TMPro.TMP_Text gemText;
+    public TMPro.TMP_Text coinText;
 
     public static int berriesCollected;
 
@@ -25,28 +27,32 @@ public class GameManager : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-{
+    {
 
-    textMeshPro.text = "" + berriesCollected;
+        berryText.text = "" + berriesCollected;
+        coinText.text = "" + coinsCollected;
+        gemText.text = "" + gemsCollected;
     
 
-}
+    }
 
 // Update is called once per frame
-void Update()
-{
-    textMeshPro.text = "" + berriesCollected;
-
-    if (berriesCollected == 8 && gemsCollected == 3 && coinsCollected == 5)
+    void Update()
     {
+        berryText.text = berriesCollected.ToString() + "/08";
+        coinText.text = coinsCollected.ToString() + "/05";
+        gemText.text = gemsCollected.ToString() + "/03";
+
+        if (berriesCollected == 8 && gemsCollected == 3 && coinsCollected == 5)
+        {
             Debug.Log("You have all the items take them to the castle");
-    }
+        }
 
-    if (MainCountdown.secondsLeft == 0)
-    {
-        //Debug.Log("Time's Up!");
-        SceneManager.LoadScene("EndGame", LoadSceneMode.Single);
+        if (MainCountdown.secondsLeft == 0)
+        {
+            //Debug.Log("Time's Up!");
+            SceneManager.LoadScene("EndGame", LoadSceneMode.Single);
 
+        }
     }
-}
 }
---
+++ Assets/Scripts/WellToGetKey.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WellToGetKey : MonoBehaviour
+{
+    public GameObject depositPanel;
+    public GameObject player;
+    public GameObject keyDungeon;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        depositPanel.SetActive(false);
+        keyDungeon.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        /*if(GameManager.berriesCollected == 8 && GameManager.gemsCollected == 3 && GameManager.coinsCollected == 5)
+        {
+            Debug.Log("You have all the requirements for the key");
+        }*/
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            depositPanel.SetActive(true);
+
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                if (GameManager.berriesCollected == 8 && GameManager.gemsCollected == 3 && GameManager.coinsCollected == 5)
+                {
+                    Debug.Log("You have all the requirements for the key");
+                    keyDungeon.SetActive(true);
+                } else
+                {
+                    Debug.Log("You don't have enough materials");
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            depositPanel.SetActive(false);
+        }
+    }
+
+}
